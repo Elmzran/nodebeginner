90259d3b94d3e75ab8edc2afe95f2ec4b16598bf
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2015-08-15 14:39:03 -0500

    Add sleep functionality to demonstrate blocking.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,13 @@
 // Request handler functions
 function start() {
 	console.log("Request handler 'start' was called.");
+	
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();
+		while (new Date().getTime() < startTime + milliSeconds);
+	}
+	
+	sleep(10000);
 	return "Hello Start";
 }
 
