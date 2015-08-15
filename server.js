a3ecd3fff67d282c0317d78a2399bf228877f243
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2015-08-14 23:35:52 -0500

    Change server request event to non-anonymous function, and encapsulate code into a single function for module exporting.

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // Import HTTP module
 var http = require("http");
 
-// Initialize the HTTP server for the application project
-http.createServer(function(request, response) {
-	console.log("Request received.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();	
-	}).listen(8888);
-	
-console.log("Server has started.");
+// Package server code into a single function for exporting
+function start() {
+	// Function for handling server connections
+	function onRequest(request, response) {
+		console.log("Request received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();	
+	}
+	// Initialize the HTTP server for the application project
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;
