047cd7a5811376fa99fd89811b8c187ec706cf00
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2015-08-16 20:09:45 -0500

    Add HTML data for response on start handler.

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,11 +5,24 @@ var exec = require("child_process").exec;
 function start(response) {
 	console.log("Request handler 'start' was called.");
 	
-	exec("ls -lah", function (error, stdout, stderr) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
-		response.end();
-	});
+	// Create HTML for startpage response
+	var body = "<html>" +
+	"<head>" +
+		"<meta http-equiv=\"Content-Type\" content=\"text/html; " +
+		"charset=UTF-8\" />" +
+	"</head>" +
+	"<body>" +
+		"<form action=\"/upload\" method=\"post\">" +
+			"<textarea name=\"text\" rows=\"20\" cols=\"60\"></textarea>" +
+			"<input type=\"submit\" value=\"Submit text\" />" +
+		"</form>" +
+	"</body>" +
+	"</html>";
+	
+	// Write HTML to page
+	response.writeHead(200, {"Content-type": "text/html"});
+	response.write(body);
+	response.end();
 }
 
 function upload(response) {
