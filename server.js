b67b8c01316a2a57b553a4585c8425a9a57e5b8c
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2015-08-18 00:02:54 -0500

    Add event listeners for handling POST data and passing POST data to the router for request handling.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var url = require("url");
 function start(route, handle) {
 	// Function for handling server connections
 	function onRequest(request, response) {
+		// Initialize variable for POST data
+		var postData = "";
+		
 		// Parse the url for the pathname
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
@@ -13,8 +16,17 @@ function start(route, handle) {
 		// Set encoding
 		request.setEncoding("utf8");
 		
-		// Execute routing		
-		route(handle, pathname, response);
+		// Add listener for concating POST data
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" +
+			postDataChunk + "'.");
+		});
+		
+		// Add listener for calling router upon POST data concation completition
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
 	}
 	// Initialize the HTTP server for the application project
 	http.createServer(onRequest).listen(8888);
